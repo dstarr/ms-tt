4323ee9d30776cce7c87789375aef62af75ea1fc
dstarr <david@elegantcode.com>
2018-02-19 12:17:14 -0800

    Final solution to the demo

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const PageNotFound = (props) => {
+const PageNotFound = () => {
 
-    const style404 = {
+const style404 = {
         fontSize: 72,
         fontWeight: 'bold'
     };
 
     return (
         <div style={{textAlign:'center' }}>
-            <h1>Page not found</h1>
+            <h1>Page not Found</h1>
             <div style={style404}>404</div>
             <img src={'/assets/404lost.jpg'} />
         </div>
     );
-};
-
-PageNotFound.propTypes = {
-    text: PropTypes.string
 };
 
 export default PageNotFound;
