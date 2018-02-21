489b95a727cbcaf47de6c2bfa5d64b78f2ac9a36
dstarr <david@elegantcode.com>
2018-02-20 20:12:51 -0800

    Adding Unicorn2

---
+++ src/components/unicorn/Unicorn2.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Button, Image} from 'react-bootstrap';
+
+class Unicorn2 extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            direction: 'left'
+        }
+    }
+
+    render = () => {
+
+        return (
+            <div style={{textAlign: 'center'}}>
+                <Image src={`/assets/unicorn_${this.state.direction}.png`} width={400}/>
+                <div>
+                    <Button onClick={this.onHandleButtonClick}>Toggle Direction</Button>
+                </div>
+
+            </div>
+        );
+    };
+
+    onHandleButtonClick = () => {
+
+        let direction = this.state.direction === 'left' ? 'right' : 'left';
+
+        this.setState({
+            direction
+        });
+    };
+
+
+}
+
+Unicorn2.propTypes = {};
+
+export default Unicorn2;
